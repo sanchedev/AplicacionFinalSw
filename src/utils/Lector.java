04b650b5b92a486dd9f279d8eb174a8b428cbe93
sanchedev <sanchedev@outlook.com>
2026-06-16 17:14:39 -0500

    feat: Agregar metodos preguntar con valores por defecto

--- src/utils/Lector.java
+++ src/utils/Lector.java
@@ -15,8 +15,21 @@ public class Lector {
         System.out.print(pregunta+ ": ");
         return sc.nextLine();
     }
+    public static String preguntar(String pregunta, String porDefecto) {
+        System.out.print(pregunta + " (defecto: "+porDefecto+"): ");
+        String respuesta = sc.nextLine();
+        if (respuesta.isEmpty()) return porDefecto;
+        return respuesta;
+    }
     public static int preguntarEntero(String pregunta) {
         return Integer.parseInt(preguntar(pregunta));
+    }
+    public static int preguntarEntero(String pregunta, int porDefecto) {
+        String respuesta = preguntar(pregunta, Integer.toString(porDefecto));
+        return Integer.parseInt(respuesta);
+    }
+    public static double preguntarDecimal(String pregunta) {
+        return Double.parseDouble(preguntar(pregunta));
     }
     public static boolean confirmar(String pregunta) {
         System.out.print(pregunta+ " (S/N): ");
